1819360e006f5353b83f87ebe4a11ad9dd9a8ad7
Kelzinha000 <138785661+Kelzinha000@users.noreply.github.com>
2024-03-03 21:12:05 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -29,23 +29,26 @@ btn.addEventListener('click',()=>{
   const select = document.getElementById("select").value
 
  const url = `https://www.abibliadigital.com.br/api/books/?abbrev=${select}`
-
  fetch(url)
- .then((response)=>{
-    if(!response.ok){
-        throw new Error (`Erro ao selecionar ao retornar`)
-     }
-     return response.json()
-})
- .then((data)=>{
-  console.log(data)
- })
- .catch((err)=>console.error(err))
- });
+ document.getElementById("resposta").innerHTML = `${url.name}`
+});
 
- function imprimeResposta(detalhes){
-    const resposta = document.getElementById('resposta')
-    resposta.innerHTML = verse.value
- }
+//  fetch(url)
+//  .then((response)=>{
+//     if(!response.ok){
+//         throw new Error (`Erro ao selecionar ao retornar`)
+//      }
+//      return response.json()
+// })
+//  .then((data)=>{
+//   console.log(data)
+//  })
+//  .catch((err)=>console.error(err))
+//  });
+
+//  function imprimeResposta(detalhes){
+//     const resposta = document.getElementById('resposta')
+//     resposta.innerHTML = verse.value
+//  }
     
 
